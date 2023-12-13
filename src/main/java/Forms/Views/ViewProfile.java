9670841c5d479bc7d6dc5efb621a6092c0213172
Diego <134670228+UjumDQ@users.noreply.github.com>
2023-12-13 12:22:55 -0500

    Add profile section without logic

--- src/main/java/Forms/Views/ViewProfile.java
+++ src/main/java/Forms/Views/ViewProfile.java
@@ -12,9 +12,35 @@ public class ViewProfile extends JPanel {
     private JLabel jlEditProfile;
     private JPanel jpForm;
     private JPanel jpFirstname;
-    private JTextField textField1;
-    private JPanel jpLFirstname;
+    private JTextField jtfFirstname;
+    private JPanel jplFirstname;
     private JLabel jlFirsname;
+    private JPanel jpLastname;
+    private JPanel jplLastname;
+    private JLabel jlLastname;
+    private JPanel jptfLastname;
+    private JTextField jtfLastname;
+    private JPanel jptfFirstname;
+    private JPanel jpGender;
+    private JPanel jplGender;
+    private JLabel jlGender;
+    private JPanel jpoGenders;
+    private JRadioButton jrbtnMale;
+    private JRadioButton jrbtnFemale;
+    private JRadioButton jrbtnNonBinary;
+    private JPanel jpDay;
+    private JComboBox jcboxDay;
+    private JLabel jlBirthday;
+    private JLabel jlContactNumber;
+    private JLabel jlSalary;
+    private JLabel jlJobTitle;
+    private JLabel jlYearsOfExperience;
+    private JButton jbtnAcceptChanges;
+    private JPanel jpBirthday;
+    private JPanel jpContactNumber;
+    private JPanel jpJobTitle;
+    private JPanel jpSalary;
+    private JPanel jpYearsOfExperience;
     private Teacher loggedTeacher = new Teacher();
 
 
@@ -27,11 +53,43 @@ public class ViewProfile extends JPanel {
     }
 
     public void initStyles() {
+
         jlEditProfile.putClientProperty( "FlatLaf.style", "font: 20 bold $light.font" );
         jlEditProfile.setForeground(Color.WHITE);
 
+        jpFirstname.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
         jlFirsname.putClientProperty("FlatLaf.style", "font: 16 $light.font");
         jlFirsname.setForeground(Color.BLACK);
+
+        jpLastname.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        jlLastname.putClientProperty("FlatLaf.style", "font: 16 $light.font");
+        jlLastname.setForeground(Color.BLACK);
+
+        jpGender.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        jlGender.putClientProperty("FlatLaf.style", "font: 16 $light.font");
+        jlGender.setForeground(Color.BLACK);
+
+        jpBirthday.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        jlBirthday.putClientProperty("FlatLaf.style", "font: 16 $light.font");
+        jlBirthday.setForeground(Color.BLACK);
+
+        jpContactNumber.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        jlContactNumber.putClientProperty("FlatLaf.style", "font: 16 $light.font");
+        jlContactNumber.setForeground(Color.BLACK);
+
+        jpJobTitle.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        jlJobTitle.putClientProperty("FlatLaf.style", "font: 16 $light.font");
+        jlJobTitle.setForeground(Color.BLACK);
+
+        jpSalary.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        jlSalary.putClientProperty("FlatLaf.style", "font: 16 $light.font");
+        jlSalary.setForeground(Color.BLACK);
+
+        jpYearsOfExperience.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        jlYearsOfExperience.putClientProperty("FlatLaf.style", "font: 16 $light.font");
+        jlYearsOfExperience.setForeground(Color.BLACK);
+
+
 
         //jlTodayIs.setText("Today is " + today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault()) + ", " + String.valueOf(today.getDayOfMonth()) + " " + today.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()) + ", " + String.valueOf(today.getYear()));
         //jlTodayIs.putClientProperty( "FlatLaf.style", "font: 24 italic $light.font" );
